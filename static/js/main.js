ff55163dab2bfcf7332add2c443659f68bf5cba9
OmarAli258 <mshf7404@leeds.ac.uk>
2026-05-07 16:04:41 +0000

    add

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,8 @@
 const quotes=["Discipline Is The Gateway To Your Dreams","Dont Dream Of Winning Train For it","Get Comfortable With Being Uncomfortable","Make Excuses Or Make Progress"]
+//above are the quotes in the banner, 2 i thought up myself and 2 are from google
 const quoteelement= document.getElementById("quotes")
 let index = 0
+//this is to the function that goes to the next quote every 5 secs and uses module to restart after reaching end of array
 setInterval(function() {
     index=(index + 1) %quotes.length
     quoteelement.textContent=quotes[index]
